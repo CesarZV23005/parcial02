784c5ef8a8dcefc9ffb074a6a2fabc2fccafe901
CesarZV23005 <zv23005@ues.edu.sv>
2025-10-02 11:41:20 -0600

    feature:trabajo dentro de la conexion y formulario para envio de datos a la db

--- conexiondb/conexion.php
+++ conexiondb/conexion.php
@@ -40,15 +40,3 @@ if($stmt->execute((array)$alumno_obj)){
     echo "INSERTADO";
 }
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>

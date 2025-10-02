e8c6a81c30f6151dac90f70ed8c9ea8bac361d53
CesarZV23005 <zv23005@ues.edu.sv>
2025-10-02 11:29:38 -0600

    feature:levantamiento del compose, funcionalidad dentro del mvc y correcta conexion de db

---
+++ conexiondb/conexion.php
@@ -0,0 +1,54 @@
+<?php
+class Database {
+    private $host = "db";
+    private $db_name = "db_alumnos";
+    private $username = "root";
+    private $password = "rootpass";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "conectado";
+        } catch(PDOException $exception) {
+            echo "Error de conexión: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+class Alumno{
+    public $nombre;
+    public $telefono;
+    public $fecha;
+    public $direccion;
+}
+
+$database = new Database();
+$conn_temp = $database->getConnection();
+$stmt = $conn_temp->prepare("INSERT INTO tbl_alumno(nombre,telefono,fecha,direccion) VALUES(:nombre,:telefono,:fecha,:direccion)");
+
+$alumno_obj = new Alumno();
+$alumno_obj->nombre = "Cesar";
+$alumno_obj->telefono = "0000-0000";
+$alumno_obj->fecha = "04-11-2005";
+$alumno_obj->direccion = "UES";
+
+if($stmt->execute((array)$alumno_obj)){
+    echo "INSERTADO";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
--- proyecto-mvc/app/views/HomeView.php
+++ proyecto-mvc/app/views/HomeView.php
@@ -1,3 +1,9 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == $_POST){
+    
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +12,21 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>VISTA MVC</h1>
-    
+    <div>
+        <h1>Registro de datos</h1>
+        <p>Ingrese su datos para continuar al calculo</p>
+        <form method="post">
+            <label>Ingrese su nombre</label>
+            <input type="text" placeholder="Nombre"><br>
+            <br>
+            <label>Ingrese su correo electronico</label>
+            <input type="text" placeholder="direccion@gmail.com"><br>
+            <br>
+            <label>Ingrese su DUI</label>
+            <input type="text" placeholder="0000000-0"><br>
+            <br>
+            <button type="submit">REGISTRAR</button>
+        </form>
+    </div>    
 </body>
 </html>
